5e067269973327800cd79d0ffa1b758da1805375
eande-jasonc1025 <67285598+eande-jasonc1025@users.noreply.github.com>
2023-10-01 19:27:21 -0700

    23-1001-1930

--- main.ts
+++ main.ts
@@ -74,42 +74,6 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    quest_Note_3.quest_Show_String_For_Note_Big_Fn(
-    "Block Coding Special Notes"
-    )
-    if (true) {
-        quest_Note_2.quest_Show_String_For_Note_Big_Fn(
-        "'If true then' Block also for modular organization and..."
-        )
-        quest_Note_2.quest_Show_String_For_Note_Big_Fn(
-        "... convenient 'copy/paste/delete' of a group of blocks"
-        )
-    }
-    if (true) {
-        quest_Note_2.quest_Show_String_For_Note_Big_Fn(
-        "In-Line Comments w/ Multiple-Colors for Varying Purposes and Priorities..."
-        )
-        quest_Note_2.quest_Show_String_For_Note_Big_Fn(
-        "... Suggested Uses:"
-        )
-        quest_Note_1.quest_Show_String_For_Note_Small_Fn(
-        "Comment: Priority Lo"
-        )
-        quest_Note_2.quest_Show_String_For_Note_Small_Fn(
-        "Comment: Priority Mi"
-        )
-        quest_Note_3.quest_Show_String_For_Note_Small_Fn(
-        "Comment: Priority Hi"
-        )
-        quest_Note_4.quest_Show_String_For_Note_Small_Fn(
-        "Question?: Priority Hi!"
-        )
-        quest_Note_5.quest_Show_String_For_Note_Small_Fn(
-        "Urgent TODO: Priority Hi!! "
-        )
-    }
-})
-basic.forever(function () {
     if (botMode_Int == 2) {
         if (true) {
             quest_Note_3.quest_Show_String_For_Note_Small_Fn(
@@ -125,6 +89,36 @@ basic.forever(function () {
             quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
             50,
             0
+            )
+            quest_Note_1.quest_Show_String_For_Note_Small_Fn(
+            "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
+            )
+            basic.showLeds(`
+                . # . # .
+                . . . # .
+                . . . # .
+                . . # # #
+                . . . # .
+                `)
+        }
+    }
+})
+basic.forever(function () {
+    if (botMode_Int == 1) {
+        if (true) {
+            quest_Note_3.quest_Show_String_For_Note_Small_Fn(
+            "Servo_Motors: Right Only"
+            )
+            quest_Note_2.quest_Show_String_For_Note_Small_Fn(
+            "50% Power for Medium Speed"
+            )
+            quest_Note_2.quest_Show_String_For_Note_Small_Fn(
+            "0% Power for Stop"
+            )
+            quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(
+            quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+            0,
+            50
             )
             quest_Note_1.quest_Show_String_For_Note_Small_Fn(
             "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
