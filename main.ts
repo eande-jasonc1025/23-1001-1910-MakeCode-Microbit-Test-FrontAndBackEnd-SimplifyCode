7c55be7e0075a9ef57db5c13510ddd060a367da8
eande-jasonc1025 <67285598+eande-jasonc1025@users.noreply.github.com>
2023-10-01 19:44:13 -0700

    23-1001-1945 TYJ

--- main.ts
+++ main.ts
@@ -2,16 +2,6 @@ input.onButtonPressed(Button.A, function () {
     botMode_Int = 1
     basic.showNumber(1)
 })
-input.onButtonPressed(Button.AB, function () {
-    botMode_Int = 3
-    basic.showIcon(IconNames.Happy)
-    basic.showNumber(3)
-})
-input.onButtonPressed(Button.B, function () {
-    botMode_Int = 2
-    basic.showIcon(IconNames.Happy)
-    basic.showNumber(2)
-})
 let botMode_Int = 0
 if (true) {
     quest_Note_3.quest_Show_String_For_Note_Small_Fn(
@@ -49,30 +39,7 @@ if (true) {
 if (true) {
     botMode_Int = 0
 }
-basic.forever(function () {
-    if (botMode_Int == 3) {
-        if (true) {
-            quest_Note_3.quest_Show_String_For_Note_Small_Fn(
-            "Servo_Motors: All Stop"
-            )
-            quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(
-            quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
-            0,
-            0
-            )
-            quest_Note_1.quest_Show_String_For_Note_Small_Fn(
-            "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
-            )
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-        }
-    }
-})
+
 basic.forever(function () {
     if (botMode_Int == 2) {
         if (true) {
@@ -103,33 +70,4 @@ basic.forever(function () {
         }
     }
 })
-basic.forever(function () {
-    if (botMode_Int == 1) {
-        if (true) {
-            quest_Note_3.quest_Show_String_For_Note_Small_Fn(
-            "Servo_Motors: Right Only"
-            )
-            quest_Note_2.quest_Show_String_For_Note_Small_Fn(
-            "50% Power for Medium Speed"
-            )
-            quest_Note_2.quest_Show_String_For_Note_Small_Fn(
-            "0% Power for Stop"
-            )
-            quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(
-            quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
-            0,
-            50
-            )
-            quest_Note_1.quest_Show_String_For_Note_Small_Fn(
-            "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
-            )
-            basic.showLeds(`
-                . # . # .
-                . . . # .
-                . . . # .
-                . . # # #
-                . . . # .
-                `)
-        }
-    }
-})
+
--- test.ts
+++ test.ts
@@ -1,1 +1,74 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+basic.showIcon(IconNames.No)
+basic.pause(2000)
+basic.forever(function () {
+	
+})
+
+input.onButtonPressed(Button.B, function () {
+    botMode_Int = 2
+    basic.showIcon(IconNames.Happy)
+    basic.showNumber(2)
+})
+
+input.onButtonPressed(Button.AB, function () {
+    botMode_Int = 3
+    basic.showIcon(IconNames.Happy)
+    basic.showNumber(3)
+})
+
+
+basic.forever(function () {
+    if (botMode_Int == 1) {
+        if (true) {
+            quest_Note_3.quest_Show_String_For_Note_Small_Fn(
+                "Servo_Motors: Right Only"
+            )
+            quest_Note_2.quest_Show_String_For_Note_Small_Fn(
+                "50% Power for Medium Speed"
+            )
+            quest_Note_2.quest_Show_String_For_Note_Small_Fn(
+                "0% Power for Stop"
+            )
+            quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(
+                quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+                0,
+                50
+            )
+            quest_Note_1.quest_Show_String_For_Note_Small_Fn(
+                "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
+            )
+            basic.showLeds(`
+                . # . # .
+                . . . # .
+                . . . # .
+                . . # # #
+                . . . # .
+                `)
+        }
+    }
+})
+
+basic.forever(function () {
+    if (botMode_Int == 3) {
+        if (true) {
+            quest_Note_3.quest_Show_String_For_Note_Small_Fn(
+                "Servo_Motors: All Stop"
+            )
+            quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(
+                quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+                0,
+                0
+            )
+            quest_Note_1.quest_Show_String_For_Note_Small_Fn(
+                "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
+            )
+            basic.showLeds(`
+                . # . # .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
+})
