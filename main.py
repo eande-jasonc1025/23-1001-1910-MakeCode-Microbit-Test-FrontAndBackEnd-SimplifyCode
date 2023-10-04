2d8434a4bacc99737dff52052bdd898b26e1807b
eande-jasonc1025 <67285598+eande-jasonc1025@users.noreply.github.com>
2023-10-03 20:54:42 -0700

    23-1003-2100 Cleanup TYJ

--- main.py
+++ main.py
@@ -1,51 +1,47 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+def on_button_pressed_a():
+    global botMode_Int
     botMode_Int = 1
-    basic.showNumber(1)
-})
-let botMode_Int = 0
-if (true) {
-    quest_Note_3.quest_Show_String_For_Note_Small_Fn("Download/Flash Confirmation Icon")
-    basic.showIcon(IconNames.Happy)
-    quest_Note_3.quest_Show_String_For_Note_Small_Fn("Continue Current State for Time Below")
-    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Fn(2, quest_Time_Units_Enum.Seconds)
-}
+    basic.show_number(1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
-if (true) {
+botMode_Int = 0
+if True:
+    quest_Note_3.quest_Show_String_For_Note_Small_Fn("Download/Flash Confirmation Icon")
+    basic.show_icon(IconNames.HAPPY)
+    quest_Note_3.quest_Show_String_For_Note_Small_Fn("Continue Current State for Time Below")
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Fn(2, quest_Time_Units_Enum.SECONDS)
+if True:
     quest_Note_3.quest_Show_String_For_Note_Small_Fn("Servo_Motors: All Stop")
     quest_Note_2.quest_Show_String_For_Note_Small_Fn("0% Power for Stop")
-    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight, 0, 0)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MOTORLEFT__S0_MOTORRIGHT,
+        0,
+        0)
     quest_Note_1.quest_Show_String_For_Note_Small_Fn("'micro:bit' Viewed Upside-Down, Same for LEDs Below")
-    basic.showLeds(`
+    basic.show_leds("""
         . # . # .
         . . . . .
         . . . . .
         . . . . .
         . . . . .
-        `)
-}
-
-if (true) {
+        """)
+if True:
     botMode_Int = 0
-}
 
-basic.forever(function on_forever() {
-    if (botMode_Int == 2) {
-        if (true) {
+def on_forever():
+    if botMode_Int == 2:
+        if True:
             quest_Note_3.quest_Show_String_For_Note_Small_Fn("Servo_Motors: Left Only")
             quest_Note_2.quest_Show_String_For_Note_Small_Fn("50% Power for Medium Speed")
             quest_Note_2.quest_Show_String_For_Note_Small_Fn("0% Power for Stop")
-            quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight, 50, 0)
+            quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MOTORLEFT__S0_MOTORRIGHT,
+                50,
+                0)
             quest_Note_1.quest_Show_String_For_Note_Small_Fn("'micro:bit' Viewed Upside-Down, Same for LEDs Below")
-            basic.showLeds(`
+            basic.show_leds("""
                 . # . # .
                 . . . # .
                 . . . # .
                 . . # # #
                 . . . # .
-                `)
-        }
-        
-    }
-    
-})
+                """)
+basic.forever(on_forever)
