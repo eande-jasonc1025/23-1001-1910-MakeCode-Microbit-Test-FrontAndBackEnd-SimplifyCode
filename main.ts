be99dd8e4c48546f23658e7e9daf740d3999f8b3
eande-jasonc1025 <67285598+eande-jasonc1025@users.noreply.github.com>
2023-10-03 20:47:28 -0700

    23-1003-2100

--- main.ts
+++ main.ts
@@ -2,6 +2,60 @@ input.onButtonPressed(Button.A, function () {
     botMode_Int = 1
     basic.showNumber(1)
 })
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+        quest_Note_3.quest_Show_String_For_Note_Small_Fn(
+        "Servo_Motors: Left + Right"
+        )
+        quest_Note_2.quest_Show_String_For_Note_Small_Fn(
+        "50% Power for Medium Speed"
+        )
+        quest_Note_2.quest_Show_String_For_Note_Small_Fn(
+        "0% Power for Stop"
+        )
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        50,
+        50
+        )
+        quest_Note_1.quest_Show_String_For_Note_Small_Fn(
+        "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
+        )
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            # # # # #
+            . # . # .
+            `)
+    }
+    if (true) {
+        quest_Note_3.quest_Show_String_For_Note_Small_Fn(
+        "Continue Current State for Time Below"
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Fn(1, quest_Time_Units_Enum.Seconds)
+        quest_Note_3.quest_Show_String_For_Note_Small_Fn(
+        "Servo_Motors: All Stop"
+        )
+    }
+    if (true) {
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Fn(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        0,
+        0
+        )
+        quest_Note_1.quest_Show_String_For_Note_Small_Fn(
+        "'micro:bit' Viewed Upside-Down, Same for LEDs Below"
+        )
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
 let botMode_Int = 0
 if (true) {
     quest_Note_3.quest_Show_String_For_Note_Small_Fn(
@@ -39,7 +93,6 @@ if (true) {
 if (true) {
     botMode_Int = 0
 }
-
 basic.forever(function () {
     if (botMode_Int == 2) {
         if (true) {
@@ -70,4 +123,3 @@ basic.forever(function () {
         }
     }
 })
-
